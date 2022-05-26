c284607380d2bdecc22d96f3e0e6703936be43cc
Peter Majmesku <majmesku@publicplan.de>
2022-05-26 10:49:03 +0200

    Able to query books from api

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,60 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { getAll } from './BooksAPI'
 
 const BooksList = () => {
-  const [bookshelfes, setBookshelfes] = useState([
-    {
-      category: 'Currently Reading',
-      books: [
+
+  const [bookshelfes, setBookshelfes] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const res = await getAll()
+
+      let booksRaw = {
+        read: [],
+        wantToRead: [],
+        currentlyReading: [],
+      }
+
+      res.forEach((book) => {
+        const bookInfo = {
+          title: book.title,
+          authors: book.authors.join(', '),
+          imageUrl: book.imageLinks.thumbnail
+        }
+
+        switch (book.shelf) {
+          case 'currentlyReading':
+            booksRaw.currentlyReading.push(bookInfo);
+            break;
+          case 'wantToRead':
+            booksRaw.wantToRead.push(bookInfo);
+            break;
+          case 'read':
+            booksRaw.read.push(bookInfo);
+            break;
+          default:
+        }
+      })
+
+      setBookshelfes([
         {
-          title: 'To Kill a Mockingbird',
-          authors: 'Harper Lee',
-          imageUrl:
-            'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+          category: 'Currently Reading',
+          books: booksRaw.currentlyReading
         },
         {
-          title: "Ender's Game",
-          authors: 'Orson Scott Card',
-          imageUrl:
-            'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-        },
-      ],
-    },
-    {
-      category: 'Want to Read',
-      books: [
-        {
-          title: '1776',
-          authors: 'David McCullough',
-          imageUrl:
-            'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
+          category: 'Want to Read',
+          books: booksRaw.wantToRead
         },
         {
-          title: "Harry Potter and the Sorcerer's Stone",
-          authors: 'J.K. Rowling',
-          imageUrl:
-            'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
+          category: 'Read',
+          books: booksRaw.read
         },
-      ],
-    },
-    {
-      category: 'Read',
-      books: [
-        {
-          title: 'The Hobbit',
-          authors: 'J.R.R. Tolkien',
-          imageUrl:
-            'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
-        },
-        {
-          title: 'The Adventures of Tom Sawyer',
-          authors: 'Mark Twain',
-          imageUrl:
-            'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")',
-        },
-      ],
-    },
-  ])
+      ])
+
+    })()
+  }, [])
 
   const moveToShelf = (e) => {
     const selectedCategory = e.target.value
@@ -124,7 +123,7 @@ const BooksList = () => {
                             style={{
                               width: 128,
                               height: 193,
-                              backgroundImage: book.imageUrl,
+                              backgroundImage: 'url(' + book.imageUrl + ')',
                             }}
                           ></div>
                           <div className="book-shelf-changer">
@@ -132,9 +131,7 @@ const BooksList = () => {
                               name={book.title}
                               onChange={(e) => moveToShelf(e)}
                             >
-                              <option value="none" disabled>
-                                Move to...
-                              </option>
+                              <option value="no-selection">Move to...</option>
                               <option value="None">None</option>
                               <option value="Currently Reading">
                                 Currently Reading

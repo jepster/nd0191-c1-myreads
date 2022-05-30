6f4e105dac1747ab5eaaa5cbf0d52639a60fbc79
Peter Majmesku <majmesku@publicplan.de>
2022-05-30 08:20:54 +0200

    Routing works

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import "./App.css";
-import { useState } from "react";
+// import { useState } from "react";
 import BooksList from "./BooksList";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
-  const [showSearchPage, setShowSearchpage] = useState(false);
+  let navigate = useNavigate();
 
   return (
-    <div className="app">
-      {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => setShowSearchpage(!showSearchPage)}
-            >
-              Close
-            </a>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                placeholder="Search by title, author, or ISBN"
-              />
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <div>
+            <BooksList/>
+            <div className="open-search">
+              <a onClick={() => navigate('/search')}>Add a book</a>
             </div>
           </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
+        }
+      />
+      <Route
+        exact
+        path="/search"
+        element={
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a
+                className="close-search"
+                onClick={() => navigate('/')}
+              >
+                Close
+              </a>
+              <div className="search-books-input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Search by title, author, or ISBN"
+                />
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <BooksList/>
-          <div className="open-search">
-            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
-          </div>
-        </div>
-      )}
-    </div>
+        }
+      />
+    </Routes>
   );
 }
 

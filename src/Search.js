0ed669a3b9704d2a4d67cba9baa3f3b95f49f538
Peter Majmesku <majmesku@publicplan.de>
2022-06-01 10:21:47 +0200

    Able to move book, but not to correct category

--- src/Search.js
+++ src/Search.js
@@ -1,91 +1,88 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {getAll, update} from "./BooksAPI";
+import {getAll, search, update, get} from "./BooksAPI";
 import PropTypes from "prop-types";
-import BooksList from "./BooksList";
 
 const Search = () => {
   let navigate = useNavigate()
 
   const [bookshelfes, setBookshelfes] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+
+  async function updateBookshelfes() {
+    const res = await getAll()
+
+    let booksRaw = {
+      read: [],
+      wantToRead: [],
+      currentlyReading: [],
+    }
+
+    res.forEach((book) => {
+      const bookInfo = {
+        title: book.title,
+        authors: ((authors) => {
+          if (Array.isArray(authors) && authors.length > 0) {
+            return authors.join(', ')
+          }
+          return authors
+        })(book.authors),
+        imageUrl: book.imageLinks.thumbnail,
+        id: book.id,
+        isbnNumbers: ((book) => {
+          const isbnNumbersRaw = []
+          book.industryIdentifiers.forEach((industryIdentifier) => {
+            isbnNumbersRaw.push(industryIdentifier.identifier)
+          })
+          return isbnNumbersRaw.join(', ')
+        })(book),
+      }
+
+      switch (book.shelf) {
+        case 'currentlyReading':
+          booksRaw.currentlyReading.push(bookInfo)
+          break
+        case 'wantToRead':
+          booksRaw.wantToRead.push(bookInfo)
+          break
+        case 'read':
+          booksRaw.read.push(bookInfo)
+          break
+      }
+    })
+
+    setBookshelfes([
+      {
+        category: 'Currently Reading',
+        books: booksRaw.currentlyReading,
+      },
+      {
+        category: 'Want to Read',
+        books: booksRaw.wantToRead,
+      },
+      {
+        category: 'Read',
+        books: booksRaw.read,
+      },
+    ])
+  }
 
   useEffect(() => {
     (async () => {
-      const res = await getAll()
-
-      let booksRaw = {
-        read: [],
-        wantToRead: [],
-        currentlyReading: [],
-      }
-
-      res.forEach((book) => {
-        const bookInfo = {
-          title: book.title,
-          authors: book.authors.join(', '),
-          imageUrl: book.imageLinks.thumbnail,
-          id: book.id,
-          isbnNumbers: ((book) => {
-            const isbnNumbersRaw = []
-            book.industryIdentifiers.forEach((industryIdentifier) => {
-              isbnNumbersRaw.push(industryIdentifier.identifier)
-            })
-            return isbnNumbersRaw.join(', ')
-          })(book),
-        }
-
-        switch (book.shelf) {
-          case 'currentlyReading':
-            booksRaw.currentlyReading.push(bookInfo);
-            break;
-          case 'wantToRead':
-            booksRaw.wantToRead.push(bookInfo);
-            break;
-          case 'read':
-            booksRaw.read.push(bookInfo);
-            break;
-          default:
-        }
-      })
-
-      setBookshelfes([
-        {
-          category: 'Currently Reading',
-          books: booksRaw.currentlyReading
-        },
-        {
-          category: 'Want to Read',
-          books: booksRaw.wantToRead
-        },
-        {
-          category: 'Read',
-          books: booksRaw.read
-        },
-      ])
-
+      await updateBookshelfes()
     })()
   }, [])
 
-  const moveToShelf = (e) => {
+  const moveToShelf = async (e) => {
     const selectedCategory = e.target.value
     const selectedBookId = e.target.name
 
-    const bookToMoveWithCategory = (() => {
-      let bookToMove = {}
-      bookshelfes.filter((bookshelf) => {
-        const returnValue = bookshelf.books.filter(
-          // eslint-disable-next-line array-callback-return
-          (book) => {
-            if (book.id === selectedBookId) {
-              return book
-            }
-          },
-        )
+    let bookToMove = {}
 
-        if (returnValue.length !== 0) {
-          bookToMove = returnValue[0]
-        }
-      })
+    const bookRetrieved = await get(selectedBookId)
+
+    if (bookRetrieved.length !== 0) {
+      bookToMove = bookRetrieved
 
       switch (selectedCategory) {
         case 'Currently Reading':
@@ -99,102 +96,47 @@ const Search = () => {
           break;
       }
 
-      return {
-        category: selectedCategory,
-        book: bookToMove,
-      }
-    })()
+      (async () => {
+        await updateBookshelfes()
+      })()
+    }
 
-    let updatedBookshelfes = []
-    bookshelfes.filter((bookshelf) => {
-      let newBooksInBookshelf = bookshelf.books.filter(
-        (book) => book.id !== selectedBookId,
-      )
-
-      if (selectedCategory === bookshelf.category) {
-        newBooksInBookshelf.push(bookToMoveWithCategory.book)
-      }
-
-      const newBookshelf = {
-        category: bookshelf.category,
-        books: newBooksInBookshelf,
-      }
-      updatedBookshelfes.push(newBookshelf)
-    })
-
-    setBookshelfes(updatedBookshelfes)
   }
 
   const handleChange = async (event) => {
     const searchString = event.target.value.toLowerCase()
 
-    const res = await getAll()
-
-    let booksRaw = {
-      read: [],
-      wantToRead: [],
-      currentlyReading: [],
+    if (searchString.length < 3) {
+      return;
     }
 
-    res.forEach((book) => {
-      const bookInfo = {
-        title: book.title,
-        authors: book.authors.join(', '),
-        imageUrl: book.imageLinks.thumbnail,
-        id: book.id,
-        isbnNumbers: ((book) => {
-          const isbnNumbersRaw = []
-          book.industryIdentifiers.forEach((industryIdentifier) => {
-            isbnNumbersRaw.push(industryIdentifier.identifier)
-          })
-          return isbnNumbersRaw.join(', ')
-        })(book),
-      }
+    const res = await search(searchString)
 
-      let match = false
+    let books = []
 
-      if (bookInfo.title.toLowerCase().includes(searchString)) {
-        match = true
-      }
+    if (res.length > 0) {
+      res.forEach((book) => {
 
-      if (match === false && bookInfo.authors.toLowerCase().includes(searchString)) {
-        match = true
-      }
-
-      if (match === false && bookInfo.isbnNumbers.toLowerCase().includes(searchString)) {
-        match = true
-      }
-
-      if (match) {
-        switch (book.shelf) {
-          case 'currentlyReading':
-            booksRaw.currentlyReading.push(bookInfo);
-            break;
-          case 'wantToRead':
-            booksRaw.wantToRead.push(bookInfo);
-            break;
-          case 'read':
-            booksRaw.read.push(bookInfo);
-            break;
-          default:
+        const bookInfo = {
+          title: book.title,
+          // authors: book.authors.join(', '),
+          imageUrl: book.imageLinks.thumbnail,
+          id: book.id,
+          // isbnNumbers: ((book) => {
+          //   const isbnNumbersRaw = []
+          //   book.industryIdentifiers.forEach((industryIdentifier) => {
+          //     isbnNumbersRaw.push(industryIdentifier.identifier)
+          //   })
+          //   return isbnNumbersRaw.join(', ')
+          // })(book),
         }
-      }
-    })
 
-    setBookshelfes([
-      {
-        category: 'Currently Reading',
-        books: booksRaw.currentlyReading
-      },
-      {
-        category: 'Want to Read',
-        books: booksRaw.wantToRead
-      },
-      {
-        category: 'Read',
-        books: booksRaw.read
-      },
-    ])
+        books.push(bookInfo);
+      })
+    }
+
+
+    setSearchResults(books)
   }
 
   return (
@@ -250,6 +192,46 @@ const Search = () => {
               </div>
             </div>
           ))}
+        </ol>
+      </div>
+
+      <div className="search-books-results">
+        <h2>Search results</h2>
+        <ol className="books-grid">
+            <ol className="books-grid">
+              {searchResults.map((book) => (
+                <li key={book.id + book.title}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div
+                        className="book-cover"
+                        style={{
+                          width: 128,
+                          height: 193,
+                          backgroundImage: 'url(' + book.imageUrl + ')',
+                        }}
+                      ></div>
+                      <div className="book-shelf-changer">
+                        <select
+                          name={book.id}
+                          onChange={(e) => moveToShelf(e)}
+                        >
+                          <option value="no-selection">Move to...</option>
+                          <option value="None">None</option>
+                          <option value="Currently Reading">
+                            Currently Reading
+                          </option>
+                          <option value="Want to Read">Want to Read</option>
+                          <option value="Read">Read</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                  </div>
+                </li>
+              ))}
+          </ol>
         </ol>
       </div>
     </div>
